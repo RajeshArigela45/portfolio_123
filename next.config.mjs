69e11d5c44faa99fad903cb45e4248aa7aed0785
Rajesh Arigela <rajesharigela4gmail.com>
2024-08-12 19:27:12 -0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable image optimization for static exports
+  },
 };
 
 export default nextConfig;
